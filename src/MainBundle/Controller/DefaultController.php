5364cec9f0c677449ce2400eff6b747720cdfaf5
Ging_freecss <mouhamed.foudhaili@esprit.tn>
2017-01-25 02:00:14 +0100

    AboutUs
     page created (twig created + controlled modified)

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -8,10 +8,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
         return $this->render('MainBundle:Default:index.html.twig');
+    }
+    /**
+     * @Route("/About" , name="aboutUs")
+     */
+    public function aboutAction()
+    {
+        return $this->render('MainBundle:Default:aboutus.html.twig');
     }
 }
