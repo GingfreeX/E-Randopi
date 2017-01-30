089bfa73bf979fabb5764c60ed259eb34c8e5d93
Unknown <b.wassim1993@hotmail.fr>
2017-01-31 00:54:14 +0100

    dashboard template added

---
+++ src/Administration/AdminBundle/AdministrationAdminBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Administration\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdministrationAdminBundle extends Bundle
+{
+    
+}
---
+++ src/Administration/AdminBundle/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Administration\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:acceuil.html.twig');
+    }
+
+    public function boiteReceptionAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:boite_reception.html.twig');
+    }
+    public function calendrierAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:calendrier.html.twig');
+    }
+    public function listedemandeAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:listedemande.html.twig');
+    }
+    public function listeguideAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:listeguide.html.twig');
+    }
+    public function listemembreAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:listemembre.html.twig');
+    }
+
+    public function listpublAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:listepub.html.twig');
+    }
+
+    public function statAction()
+    {
+        return $this->render('AdministrationAdminBundle:Admin/Views:statistique.html.twig');
+    }
+}
